8d97452599d9419f6de5a46b4d09c5a816626821
ProjetEpitaBacklog <epitaprojet@gmail.com>
2026-04-11 11:46:23 +0200

    feat(contracts): new libs (#50)
    * feat(contracts): new libs
    
    * feat(contracts): new libs
    
    * feat(contracts): new libs
    
    * feat(contracts): new libs
    
    * feat(contracts): new libs
    
    ---------
    
    Co-authored-by: VictorAgahi <victor@agahi.com>

--- packages/contracts-nest/src/index.ts
+++ packages/contracts-nest/src/index.ts
@@ -2,3 +2,4 @@ export * from './common/index.js';
 export * from './event/index.js';
 export * from './post/index.js';
 export * from './user/index.js';
+export * from './grpc.helpers.js';
--- packages/domain-user/CHANGELOG.md
+++ packages/domain-user/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 2.1.1
+
+### Patch Changes
+
+- Updated dependencies []:
+  - @volontariapp/contracts@3.0.0
+
 ## 2.1.0
 
 ### Minor Changes
