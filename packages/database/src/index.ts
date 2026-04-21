0c1cb4494567b8b601ac288f73fa8ec435a5d8fa
Clément Pasteau <clement.pasteau@epita.fr>
2026-04-21 16:08:24 +0200

    fix(jobs & events): added to index.js

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,4 +3,6 @@ import 'reflect-metadata';
 export * from './core/base.repository.js';
 export * from './core/mapper.service.js';
 
+export * from './outbox/index.js';
+
 export type { Repository, ObjectLiteral } from 'typeorm';
---
+++ packages/database/src/outbox/index.ts
@@ -0,0 +1,8 @@
+export * from './entities/event-queue.entity.js';
+export * from './entities/jobs-outbox.entity.js';
+
+export * from './models/event-queue.model.js';
+export * from './models/jobs-outbox.model.js';
+
+export * from './writer/event-queue.writer.js';
+export * from './writer/jobs-outbox.writer.js';
