d5b3c1b29d187e11d47fe18df9c535452f0a63ee
VictorAgahi <victor@agahi.com>
2026-05-01 18:11:42 +0200

    feat(auth): update grpc metadata

--- packages/auth/src/services/grpc-metadata.helper.ts
+++ packages/auth/src/services/grpc-metadata.helper.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Metadata } from '@grpc/grpc-js';
-import type { JwtService } from './jwt.service.js';
+import { JwtService } from './jwt.service.js';
 import { INTERNAL_TOKEN_METADATA_KEY } from '../constants/index.js';
 import type { AuthUser } from '../interfaces/index.js';
 import { Logger } from '@volontariapp/logger';
@@ -11,8 +11,8 @@ export class GrpcMetadataHelper {
   constructor(private readonly jwtService: JwtService) {}
 
   async createInternalMetadata(user: AuthUser): Promise<Metadata> {
-    const metadata = new Metadata();
     const token = await this.jwtService.signInternal(user);
+    const metadata = new Metadata();
     metadata.set(INTERNAL_TOKEN_METADATA_KEY, token);
     this.logger.debug(`Created internal metadata for user ${user.id}`);
     return metadata;
