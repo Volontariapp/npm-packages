072b6dcaf79ac767355ac7983abc949e3ac71c74
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-05-08 18:35:55 +0200

    fix: use testing lib

--- packages/workers/src/test/utils/test-worker.utils.ts
+++ packages/workers/src/test/utils/test-worker.utils.ts
@@ -1,9 +1,13 @@
 import { jest } from '@jest/globals';
+import { createMockLogger } from '@volontariapp/testing';
+import type { Logger } from '@volontariapp/logger';
 import { BaseWorker } from '../../core/base.worker.js';
 import type { TestJob, TestJobType } from './test-job.utils.js';
-import { makeLoggerMock, type LoggerMock } from '../mocks/logger.mock.js';
+
+export type LoggerMock = jest.Mocked<Logger>;
 
 export class TestWorker extends BaseWorker<TestJobType> {
-  public override logger: LoggerMock = makeLoggerMock();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  public override logger: LoggerMock = createMockLogger<Logger>();
   public processJob = jest.fn() as jest.MockedFunction<(job: TestJob) => Promise<void>>;
 }
