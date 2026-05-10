ce2c1f19e25799aa58bead700ab71aa6d7533ba8
victor.giroud <victor.giroud@epita.fr>
2026-05-10 20:44:23 +0200

    fix : export

--- packages/domain-post/src/index.ts
+++ packages/domain-post/src/index.ts
@@ -3,5 +3,6 @@ import { registerPostMappings } from './models/mapper.js';
 export * from './entities/index.js';
 export * from './models/index.js';
 export * from './services/index.js';
+export * from './repositories/index.js';
 
 registerPostMappings();
