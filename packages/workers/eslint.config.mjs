4f2b3ea5b103ba7e242c9a0adaaddb120e8a5855
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-05-11 16:21:02 +0200

    fix eslint

--- packages/workers/eslint.config.mjs
+++ packages/workers/eslint.config.mjs
@@ -1,13 +1,3 @@
 import config from '@volontariapp/eslint-config';
 
-export default [
-  {
-    files: ['src/test/**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-useless-constructor': 'off',
-    },
-  },
-  ...config,
-];
+export default config;
